84ccb8a0e107d65e5d81aeea3de82470963045c4
Tradicans <54860251+Tradicans@users.noreply.github.com>
2023-02-28 21:03:25 -0500

    code cleanup

--- src/week7/class2/lab13component.jsx
+++ src/week7/class2/lab13component.jsx
@@ -11,6 +11,7 @@ import {
 } from "@mui/material";
 import theme from "../../theme";
 import "../../App.css";
+
 const Lab13Component = () => {
  const initialState = {
  msg: "",
@@ -19,122 +20,103 @@ const Lab13Component = () => {
  users: [],
  names: [],
  };
+
  const reducer = (state, newState) => ({ ...state, ...newState });
  const [state, setState] = useReducer(reducer, initialState);
- //const [selection, setSelection] = useState("");
+
  const onChange = async (e, selectedOption) => {
     let queryString = `query {userbyname(name: "${selectedOption}"){email}}`;
-    //let email = JSON.stringify({ query: 'query ($name: ${selectedOption}) {userbyname(name: ${selectedOption}){email} }'});
     let response = await fetch(`http://localhost:5000/graphql`, {
         method: "POST",
         headers: {
         "Content-Type": "application/json; charset=utf-8",
         },
         body: JSON.stringify({ query: queryString}),
-        
         });
-         let json = await response.json();
-        // setState({
-        // snackBarMsg: 'User data loaded',
-        // users: json.data.users,
-        // contactServer: true,
-        // names: json.data.users.map((a) => a.name),
-        // });
- let email = json.data.userbyname.email; 
- 
- 
- 
- 
+    let json = await response.json();
+    let email = json.data.userbyname.email; 
     selectedOption
- ? setState({msg: `${selectedOption} selected. This user can be contacted at ${email}`})
- : setState({msg: ""});
- //setState({msg: selection});
+        ? setState({msg: `${selectedOption} selected. This user can be contacted at ${email}`})
+        : setState({msg: ""});
  };
 
  useEffect(() => {
- fetchUsers();
- }, []);
+    fetchUsers();
+    }, []);
+
  const fetchUsers = async () => {
- try {
- setState({
- contactServer: true,
- snackBarMsg: "Attempting to load users from server...",
- });
-
-
- let response = await fetch("http://localhost:5000/graphql", {
- method: "POST",
- headers: {
- "Content-Type": "application/json; charset=utf-8",
- },
- body: JSON.stringify({ query: "query { users{name,age,email} }" }),
- });
-  let json = await response.json();
- setState({
- snackBarMsg: 'User data loaded',
- users: json.data.users,
- contactServer: true,
- names: json.data.users.map((a) => a.name),
- });
-//  let json = await response.json();
-//  setState({
-//  snackBarMsg: `users loaded`,
-//  users: json.data.users,
-//  contactServer: true,
-//  });
-
- } catch (error) {
- console.log(error);
- setState({
- msg: `Problem loading server data - ${error.message}`,
- });
- }
+    try {
+        setState({
+        contactServer: true,
+        snackBarMsg: "Attempting to load users from server...",
+        });
+        let response = await fetch("http://localhost:5000/graphql", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify({ query: "query { users{name,age,email} }" }),
+            });
+        let json = await response.json();
+        setState({
+            snackBarMsg: 'User data loaded',
+            users: json.data.users,
+            contactServer: true,
+            names: json.data.users.map((a) => a.name),
+            });
+    } catch (error) {
+        console.log(error);
+        setState({
+            msg: `Problem loading server data - ${error.message}`,
+        });
+    }
  };
+ 
  const snackbarClose = (event, reason) => {
- if (reason === "clickaway") {
- return;
- }
- setState({
- msg: `${state.users.length} users loaded`,
- //msg: `${state.names.length} names loaded`,
- contactServer: false,
- });
- };
+    if (reason === "clickaway") {
+        return;
+    }
+    setState({
+    msg: `${state.users.length} users loaded`,
+    contactServer: false,
+    });
+    };
+ 
  return (
- <ThemeProvider theme={theme}>
- <Card className="card">
- <CardHeader
- title="Lab 13 - Search For User"
- style={{ color: theme.palette.primary.main, textAlign: "center" }}
- />
- <CardContent>
- <div>
- <Autocomplete
- id="names"
- options={state.names}
- getOptionLabel={(option) => option}
- style={{ width: 300 }}
- onChange={onChange}
- renderInput={(params) => (
- <TextField
- {...params}
- label="available users"
- variant="outlined"
- fullWidth
- />
- )}
- />
- <Typography color="error">{state.msg}</Typography>
- </div>
- </CardContent>
- </Card>
- <Snackbar
- open={state.contactServer}
- message={state.snackBarMsg}
- autoHideDuration={3000}
- onClose={snackbarClose}
- />
- </ThemeProvider>
- );
+    <ThemeProvider theme={theme}>
+        <Card className="card">
+            <CardHeader
+                title="Lab 13 - Search For User"
+                style={{ color: theme.palette.primary.main, textAlign: "center" }}
+            />
+            <CardContent>
+                <div>
+                    <Autocomplete
+                        id="names"
+                        options={state.names}
+                        getOptionLabel={(option) => option}
+                        style={{ width: 300 }}
+                        onChange={onChange}
+                        renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="available users"
+                            variant="outlined"
+                            fullWidth
+                        />
+                    )}
+                    />
+                    <Typography color="error">{state.msg}</Typography>
+                </div>
+            </CardContent>
+        </Card>
+        <Snackbar
+            open={state.contactServer}
+            message={state.snackBarMsg}
+            autoHideDuration={3000}
+            onClose={snackbarClose}
+        />
+    </ThemeProvider>
+    );
 };
 export default Lab13Component;
