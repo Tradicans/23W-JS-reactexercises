83e8358e5718b3060df1f727a1925e5d6bd77651
Tradicans <54860251+Tradicans@users.noreply.github.com>
2023-02-28 22:12:05 -0500

    All tests working

--- cypress/e2e/lab13spec.cy.js
+++ cypress/e2e/lab13spec.cy.js
@@ -21,11 +21,14 @@ describe("Test Lab 13 - Load and Find User from Autocomplete", () => {
         cy.contains("Lab 13");
         cy.wait(3000); 
         cy.get("#names").type("Am{downArrow}{enter}");
+        });
+    it("confirms User was found", () => {
+        cy.visit("http://localhost:5173/");
+        cy.get("#menubtn").click();
+        cy.contains("a", "Lab 13").click();
+        cy.contains("Lab 13");
+        cy.wait(3000); 
+        cy.get("#names").type("Am{downArrow}{enter}");
         cy.contains("Amber");
         });
-    // it("confirms User was found", () => {
-    //     cy.visit("http://localhost:5173/");
-    //     cy.get("#fruits").type("banana{downArrow}{enter}");
-    //     cy.contains("You selected Banana");
-    //     });
    });
