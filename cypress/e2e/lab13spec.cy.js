4ea6d84b50de0e4715ef32029f73f5de6567951b
Tradicans <54860251+Tradicans@users.noreply.github.com>
2023-02-28 22:01:10 -0500

    4 tests working

--- cypress/e2e/lab13spec.cy.js
+++ cypress/e2e/lab13spec.cy.js
@@ -3,18 +3,26 @@ describe("Test Lab 13 - Load and Find User from Autocomplete", () => {
         cy.visit("http://localhost:5173/");
         });
     it("clicks the menu button lab 13 option", () => {
+        cy.visit("http://localhost:5173/");
         cy.get("#menubtn").click();
-        // cy.contains("a", "Lab 13").click();
+        cy.contains("a", "Lab 13").click();
         });
-    // it("confirms lab 13 component loaded", () => {
-    //     cy.contains("Lab 13");
-    //     cy.wait(3000);       
-    //     });
-    // it("types User name", () => {
-    //     cy.visit("http://localhost:5173/");
-    //     cy.get("#fruits").type("banana{downArrow}{enter}");
-    //     cy.contains("You selected Banana");
-    //     });
+    it("confirms lab 13 component loaded", () => {
+        cy.visit("http://localhost:5173/");
+        cy.get("#menubtn").click();
+        cy.contains("a", "Lab 13").click();
+        cy.contains("Lab 13");
+        cy.wait(3000);       
+        });
+    it("types User name", () => {
+        cy.visit("http://localhost:5173/");
+        cy.get("#menubtn").click();
+        cy.contains("a", "Lab 13").click();
+        cy.contains("Lab 13");
+        cy.wait(3000); 
+        cy.get("#names").type("Am{downArrow}{enter}");
+        cy.contains("Amber");
+        });
     // it("confirms User was found", () => {
     //     cy.visit("http://localhost:5173/");
     //     cy.get("#fruits").type("banana{downArrow}{enter}");
