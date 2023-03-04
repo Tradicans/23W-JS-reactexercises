b6b99b254c0dd3c85518ecc2ebbb4f80b1d6fed2
Tradicans <54860251+Tradicans@users.noreply.github.com>
2023-03-04 14:36:51 -0500

    updated fetchCountries, onChange to get alert text

--- src/project1/AdvisoryAddComponent.jsx
+++ src/project1/AdvisoryAddComponent.jsx
@@ -21,23 +21,22 @@ const AdvisoryAddComponent = () => {
         contactServer: false,
         travelername: "",
         selectedCountry: "",
+        alerts: [],
         countrynames: [],
+        alerttext: "",
+        date: "",
         };
        
         const reducer = (state, newState) => ({ ...state, ...newState });
         const [state, setState] = useReducer(reducer, initialState);
-       
         const onChange = async (e, selectedOption) => {
             selectedOption
-                ? setState({selectedCountry: selectedOption})
-                : setState({selectedCountry: ""});
+                ? setState({selectedCountry: selectedOption, alerttext: alerts[selectedOption].text})
+                : setState({selectedCountry: "", alerttext: ""});
          };
-
-
         useEffect(() => {
            fetchCountries();
            }, []);
-       
         const fetchCountries = async () => {
            try {
                setState({
@@ -49,11 +48,12 @@ const AdvisoryAddComponent = () => {
                    headers: {
                    "Content-Type": "application/json; charset=utf-8",
                    },
-                   body: JSON.stringify({ query: "query { alerts{name} }" }),
+                   body: JSON.stringify({ query: "query { alerts{name, text} }" }),
                    });
                let json = await response.json();
                setState({
                    snackBarMsg: 'loaded # countries',
+                   alerts: json.data.alerts,
                    contactServer: true,
                    countrynames: json.data.alerts.map((a) => a.name),
                    });
@@ -65,42 +65,50 @@ const AdvisoryAddComponent = () => {
            }
         };
         
-        // const onAddClicked = async () => {
-        //     let user = {
-        //     name: state.name,
-        //     age: state.age,
-        //     email: state.email,
-        //     };
-        //     let myHeaders = new Headers();
-        //     myHeaders.append("Content-Type", "application/json");
-        //     try {
-        //     let query = JSON.stringify({
-        //     query: `mutation {adduser(name: "${user.name}",age: ${user.age}, email: "${user.email}" ) 
-        //    { name, age, email }}`,
-        //     });
-        //     console.log(query);
-        //     let response = await fetch("http://localhost:5000/graphql", {
-        //     method: "POST",
-        //     headers: {
-        //     "Content-Type": "application/json; charset=utf-8",
-        //     },
-        //     body: query,
-        //     });
-        //     let json = await response.json();
-        //     setState({
-        //     showMsg: true,
-        //     snackbarMsg: `User ${json.data.adduser.name} added`,
-        //     name: "",
-        //     age: 0,
-        //     email: "",
-        //     });
-        //     } catch (error) {
-        //     setState({
-        //     snackbarMsg: `${error.message} - user not added`,
-        //     showMsg: true,
-        //     });
-        //     }
-        //     };
+        const onAddClicked = async () => {
+            let advisory = {
+            name: state.travelername,
+            country: state.selectedCountry,
+            text: state.alerttext,
+            date: state.date,
+            };
+            let myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            try {
+            let query = JSON.stringify({
+            query: `mutation {adduser(name: "${user.name}",age: ${user.age}, email: "${user.email}" ) 
+           { name, age, email }}`,
+            });
+            console.log(query);
+            let response = await fetch("http://localhost:5000/graphql", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            },
+            body: query,
+            });
+            let json = await response.json();
+            setState({
+            showMsg: true,
+            snackbarMsg: `User ${json.data.adduser.name} added`,
+            name: "",
+            age: 0,
+            email: "",
+            });
+            } catch (error) {
+            setState({
+            snackbarMsg: `${error.message} - user not added`,
+            showMsg: true,
+            });
+            }
+            };
+
+//done: schema for advisory and mutation
+//done: resolver for advisory and mutation
+//done: query for advisories
+//todo: add to AdvisoryAddComponent to track text for selected country
+//todo: add code to actually add advisory - where?
+//done: add additional db to env and conf?
 
         const handleNameInput = (e) => {
             setState({ travelername: e.target.value });
