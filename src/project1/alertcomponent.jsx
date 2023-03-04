1f8c92c453464f2669a49805a597745d11fb4a92
Tradicans <54860251+Tradicans@users.noreply.github.com>
2023-03-04 03:10:09 -0500

    project1 setup working

--- src/project1/alertcomponent.jsx
+++ src/project1/alertcomponent.jsx
@@ -1,4 +1,6 @@
 import React, { useReducer, useEffect } from "react";
+import { useQuery } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "@mui/material/styles";
 import {
  Card,
@@ -6,73 +8,93 @@ import {
  CardContent,
  Snackbar,
  Typography,
+ List,
+ ListItem,
+ ListItemText,
+ Divider,
 } from "@mui/material";
+import "../App.css";
 import theme from "./theme";
 import Project1Component from "./project1component";
+import SetupQuery from "./setupquery";
 
 const AlertComponent = () => {
- const initialState = {
- msg: "",
- snackBarMsg: "",
- contactServer: false,
- results: [],
- };
- const reducer = (state, newState) => ({ ...state, ...newState });
- const [state, setState] = useReducer(reducer, initialState);
+//  const initialState = {
+//  msg: "",
+//  snackBarMsg: "",
+//  contactServer: false,
+//  results: [],
+//  };
+//  const reducer = (state, newState) => ({ ...state, ...newState });
+//  const [state, setState] = useReducer(reducer, initialState);
  
 
  
- useEffect(() => {
- fetchResults();
- }, []);
- const fetchResults = async () => {
- try {
- setState({
- contactServer: true,
- snackBarMsg: "Running setup",
- });
- let response = await fetch("http://localhost:5000/graphql", {
- method: "POST",
- headers: {
- "Content-Type": "application/json; charset=utf-8",
- },
- body: JSON.stringify({ query: "query { project1_setup{results} }" }),
- });
- let json = await response.json();
+//  useEffect(() => {
+//  fetchResults();
+//  }, []);
+//  const fetchResults = async () => {
+//  try {
+//  setState({
+//  contactServer: true,
+//  snackBarMsg: "Running setup",
+//  });
+//  let response = await fetch("http://localhost:5000/graphql", {
+//  method: "POST",
+//  headers: {
+//  "Content-Type": "application/json; charset=utf-8",
+//  },
+//  body: JSON.stringify({ query: "query { project1_setup{results} }" }),
+//  });
+//  let json = await response.json();
 
-// return the JSON to a variable called payload, then
-let resArr = [];
-resArr = json.data.project1_setup.results
- .replace(/([.])\s*(?=[A-Z])/g, "$1|")
- .split("|");
+// // return the JSON to a variable called payload, then
+// let resArr = [];
+// resArr = json.data.project1_setup.results
+//  .replace(/([.])\s*(?=[A-Z])/g, "$1|")
+//  .split("|");
+
+//  setState({
+//  snackBarMsg: `alerts collection setup completed`,
+//  //results: json.data.project1_setup.results,
+//  results: resArr,
+//  contactServer: false,
+//  });
+//  } catch (error) {
+//  console.log(error);
+//  setState({
+//  msg: `Problem loading server data - ${error.message}`,
+//  });
+//  }
+//  };
+//  const snackbarClose = (event, reason) => {
+//  if (reason === "clickaway") {
+//  return;
+//  }
+//  setState({
+//  //msg: `${state.results.length} results loaded`,
+//  contactServer: false,
+//  });
+//  };
 
 
 
 
+// import ReactQueryExample from "./week8/reactqueryexample";
+const queryClient = new QueryClient();
+// const App = () => {
+//  return (
+//  <QueryClientProvider client={queryClient}>
+//  <ReactQueryExample />
+//  </QueryClientProvider>
+//  );
+// };
+// export default App;
 
- setState({
- snackBarMsg: `alerts collection setup completed`,
- //results: json.data.project1_setup.results,
- results: resArr,
- contactServer: true,
- });
- } catch (error) {
- console.log(error);
- setState({
- msg: `Problem loading server data - ${error.message}`,
- });
- }
- };
- const snackbarClose = (event, reason) => {
- if (reason === "clickaway") {
- return;
- }
- setState({
- msg: `${state.results.length} results loaded`,
- contactServer: false,
- });
- };
+
+
  return (
+
     
  <ThemeProvider theme={theme}>
    
@@ -83,16 +105,22 @@ resArr = json.data.project1_setup.results
  style={{ color: theme.palette.primary.main, textAlign: "center" }}
  />
  <CardContent>
- <div>
- <Typography color="error">{state.msg}</Typography>
- </div>
+  <QueryClientProvider client={queryClient}>
+  <SetupQuery />
+  </QueryClientProvider>
+
+
+
+
+
+
  </CardContent>
  </Card>
  <Snackbar
- open={state.contactServer}
- message={state.snackBarMsg}
- autoHideDuration={3000}
- onClose={snackbarClose}
+//  open={state.contactServer}
+//  message={state.snackBarMsg}
+//  autoHideDuration={3000}
+//  onClose={snackbarClose}
  />
  </ThemeProvider>
  );
---
+++ src/project1/setupquery.jsx
@@ -0,0 +1,46 @@
+import { useQuery } from "react-query";
+import { ThemeProvider } from "@mui/material/styles";
+import {
+ Card,
+ CardHeader,
+ CardContent,
+ Snackbar,
+ Typography,
+ List,
+ ListItem,
+ ListItemText,
+ Divider,
+} from "@mui/material";
+import "../App.css";
+import theme from "./theme";
+const SetupQuery = () => {
+ // useQuery returns 3 items, key name is a label for the query
+ const { isLoading, error, data } = useQuery("querykeyname", async () => {
+ let response = await fetch("http://localhost:5000/graphql", {
+ method: "POST",
+ headers: {
+ "Content-Type": "application/json; charset=utf-8",
+ },
+ body: JSON.stringify({ query: "query { project1_setup{results} }" }),
+ });
+ let json = await response.json();
+ let resArr = [];
+ resArr = json.data.project1_setup.results
+ .replace(/([.])\s*(?=[A-Z])/g, "$1|")
+ .split("|");
+ return resArr;
+ });
+ if (isLoading) return "Loading...";
+ if (error) return "An error has occurred: " + error.message;
+ return (
+<List style={{color: theme.palette.error.main}}>
+ {
+ data.map((result, index) => {
+ return <div key={index}><ListItem ><ListItemText primary={result} /></ListItem><Divider /></div>;
+ })
+ }
+ </List>
+
+ );
+};
+export default SetupQuery
