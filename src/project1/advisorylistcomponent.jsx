bf6613b9cd55edf365c9e141e0e9105110238388
Tradicans <54860251+Tradicans@users.noreply.github.com>
2023-03-08 10:05:36 -0500

    traveler button feeds autocomplete

--- src/project1/advisorylistcomponent.jsx
+++ src/project1/advisorylistcomponent.jsx
@@ -16,15 +16,83 @@ import Project1Component from "./project1component";
 const AdvisoryListComponent = (props) => {
 
     const initialState = {
-
+        autoLabel: "Traveler",
+        autoOptions: [],
+        selectedOption: "",
+        //optionsQuery: "",
     };
     const reducer = (state, newState) => ({ ...state, ...newState });
     const [state, setState] = useReducer(reducer, initialState);
+    useEffect(() => {
+        travelerClicked();
+        }, []);
     
     const sendSnack = (msg) => {
         props.dataFromList(msg);
         };
+        const onChange = async (e, selectedOption) => {
 
+            // let queryString = `query {alertbycountry(name: "${selectedOption}"){text}}`;
+            // let response = await fetch(`http://localhost:5000/graphql`, {
+            //     method: "POST",
+            //     headers: {
+            //     "Content-Type": "application/json; charset=utf-8",
+            //     },
+            //     body: JSON.stringify({ query: queryString}),
+            //     });
+            // let json = await response.json();
+
+            // let alertText = json.data.alertbycountry.text;
+            // selectedOption
+            //     ? setState({selectedCountry: selectedOption, alerttext: alertText})
+            //     : setState({selectedCountry: "", alerttext: ""});
+         };
+         const autoCompleteOptionsQuery = async (queryString) => {
+            let myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            try {
+            let query = JSON.stringify({
+            query: queryString,
+            });
+            console.log(query);
+            let response = await fetch("http://localhost:5000/graphql", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            },
+            body: query,
+            });
+            let json = await response.json();
+            return json;
+            
+            } catch (error) {
+            sendSnack(`Problem loading server data - ${error.message}`);
+            }
+         };
+const travelerClicked = async () => {
+    let query = `query{travelers}`;
+setState({
+    autoLabel: "Traveler",
+});
+sendSnack(`Attempting to load travelers from server...`);
+try{
+    let json = await autoCompleteOptionsQuery(query);
+    setState({
+        autoOptions: json.data.travelers,
+        });
+        sendSnack(`${state.autoOptions.length} travelers found`);
+
+} catch (error) {
+    sendSnack(`Problem loading server data - ${error.message}`);
+    }
+    
+};
+const regionClicked = async () => {
+    setState({autoLabel: "Region"});
+    };
+    const subregionClicked = async () => {
+        setState({autoLabel: "Sub-Region"});
+        };
 
 return (
     <ThemeProvider theme={theme}>
@@ -40,7 +108,7 @@ return (
                     style={{marginInline: "2vw"}}                      
                     color="secondary"
                     variant="contained"
-                    // onClick={travelerClicked}
+                    onClick={travelerClicked}
                 >
                 Traveler
                 </Button>
@@ -48,7 +116,7 @@ return (
                     style={{marginInline: "2vw"}}
                     color="secondary"
                     variant="contained"
-                    // onClick={regionClicked}
+                    onClick={regionClicked}
                     >
                     Region
                 </Button>
@@ -56,10 +124,28 @@ return (
                     style={{marginInline: "2vw"}}
                     color="secondary"
                     variant="contained"
-                    // onClick={subregionClicked}
+                    onClick={subregionClicked}
                     >
                     Sub-Region
                 </Button>
+            </div>
+            <div>
+                <Autocomplete
+                    options={state.autoOptions}
+                    getOptionLabel={(option) => option}
+                    style={{ padding: "1vh" }}
+                               onChange={onChange}
+                               value={state.selectedOption}
+                               renderInput={(params) => (
+                               <TextField
+                                   {...params}
+                                   label={state.autoLabel}
+                                   variant="outlined"
+                                   fullWidth
+                                   
+                               />
+                               )}
+                           />
             </div>
         </CardContent>
         </Card>
