4897fd9b4829d8596db86ccb453739edd627ddfd
Tradicans <54860251+Tradicans@users.noreply.github.com>
2023-03-08 12:16:29 -0500

    traveler alerts table working

--- src/project1/advisorylistcomponent.jsx
+++ src/project1/advisorylistcomponent.jsx
@@ -8,6 +8,12 @@ import {
  Typography,
  TextField,
  Button,
+ Table,
+ TableBody,
+ TableCell,
+ TableContainer,
+ TableHead,
+ TableRow,
 } from "@mui/material";
 import theme from "./theme";
 import "../App.css";
@@ -18,141 +24,198 @@ const AdvisoryListComponent = (props) => {
     const initialState = {
         autoLabel: "Traveler",
         autoOptions: [],
-        selectedOption: "",
-        //optionsQuery: "",
+        selectedOption: ``,
+        selectedAlerts: [],
     };
     const reducer = (state, newState) => ({ ...state, ...newState });
     const [state, setState] = useReducer(reducer, initialState);
     useEffect(() => {
         travelerClicked();
         }, []);
-    
     const sendSnack = (msg) => {
         props.dataFromList(msg);
         };
-        const onChange = async (e, selectedOption) => {
-
-            // let queryString = `query {alertbycountry(name: "${selectedOption}"){text}}`;
-            // let response = await fetch(`http://localhost:5000/graphql`, {
-            //     method: "POST",
-            //     headers: {
-            //     "Content-Type": "application/json; charset=utf-8",
-            //     },
-            //     body: JSON.stringify({ query: queryString}),
-            //     });
-            // let json = await response.json();
-
-            // let alertText = json.data.alertbycountry.text;
-            // selectedOption
-            //     ? setState({selectedCountry: selectedOption, alerttext: alertText})
-            //     : setState({selectedCountry: "", alerttext: ""});
-         };
-         const autoCompleteOptionsQuery = async (queryString) => {
-            let myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-            try {
+    const onChange = async (e, selectedOption) => {
+        let queryString = ``;
+        let queryName = ``;
+        switch(state.autoLabel) {
+            case "Traveler":
+                queryName = `advisoriesbytraveler`
+                queryString = `query {advisoriesbytraveler(name: "${selectedOption}"){country,text,date}}`;
+                break;
+            case "Region":
+                queryName = `alertsforregion`;
+                queryString = '';
+                break;
+            case "Sub-Region":
+                queryName = ``;
+                queryString = '';
+                break;
+            default:
+                return null;
+        }
+        try {
+            let json = await autoCompleteOptionsQuery(queryString);
+            selectedOption
+                ? setState({selectedOption: selectedOption, selectedAlerts: json.data[queryName]})
+                : setState({selectedOption: "", selectedAlerts: []});
+        }
+        catch{
+            sendSnack(`Problem loading server data - ${error.message}`);
+            setState({selectedOption: "", selectedAlerts: []});
+        }
+        };
+    const autoCompleteOptionsQuery = async (queryString) => {
+        let myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        try {
             let query = JSON.stringify({
-            query: queryString,
+                query: queryString,
             });
             console.log(query);
             let response = await fetch("http://localhost:5000/graphql", {
-            method: "POST",
-            headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            },
-            body: query,
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                },
+                body: query,
             });
             let json = await response.json();
             return json;
-            
-            } catch (error) {
+        } catch (error) {
             sendSnack(`Problem loading server data - ${error.message}`);
-            }
-         };
-const travelerClicked = async () => {
-    let query = `query{travelers}`;
-setState({
-    autoLabel: "Traveler",
-});
-sendSnack(`Attempting to load travelers from server...`);
-try{
-    let json = await autoCompleteOptionsQuery(query);
-    setState({
-        autoOptions: json.data.travelers,
-        });
-        sendSnack(`${state.autoOptions.length} travelers found`);
-
-} catch (error) {
-    sendSnack(`Problem loading server data - ${error.message}`);
-    }
-    
-};
-const regionClicked = async () => {
-    setState({autoLabel: "Region"});
+        }
     };
-    const subregionClicked = async () => {
-        setState({autoLabel: "Sub-Region"});
+    const travelerClicked = async () => {
+        let query = `query{travelers}`;
+        setState({autoLabel: "Traveler"});
+        sendSnack(`Attempting to load travelers from server...`);
+        try{
+            let json = await autoCompleteOptionsQuery(query);
+            setState({autoOptions: json.data.travelers});
+            sendSnack(`${json.data.travelers.length} travelers found`);
+        } catch (error) {
+            sendSnack(`Problem loading server data - ${error.message}`);
+        }
+    };
+    const regionClicked = async () => {
+        let query = `query{regions}`;
+        setState({autoLabel: "Region"});
+        sendSnack(`Attempting to load regions from server...`);
+        try{
+            let json = await autoCompleteOptionsQuery(query);
+            setState({autoOptions: json.data.regions});
+            sendSnack(`${json.data.regions.length} regions found`);
+        } catch (error) {
+            sendSnack(`Problem loading server data - ${error.message}`);
+        }
         };
+    const subregionClicked = async () => {
+        let query = `query{subregions}`;
+        setState({autoLabel: "Sub-Region"});
+        sendSnack(`Attempting to load regions from server...`);
+        try{
+            let json = await autoCompleteOptionsQuery(query);
+            setState({autoOptions: json.data.subregions});
+            sendSnack(`${json.data.subregions.length} subregions found`);
+        } catch (error) {
+            sendSnack(`Problem loading server data - ${error.message}`);
+        }
+        };
+    const renderContent = React.useCallback(() => {
+        if(state.selectedOption) {
+            const alerts = state.selectedAlerts;
+            return ( 
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Country</TableCell>
+                                <TableCell align="left">Alert Information</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {alerts.map((alert) => (
+                                <TableRow key={alert.index}>
+                                    <TableCell>{alert.country}</TableCell>
+                                    <TableCell>{alert.text} {alert.date}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
 
-return (
-    <ThemeProvider theme={theme}>
-        <Card className="card">
-        <Project1Component  />
-        <CardHeader
-            title="List Advisories"
-            style={{ color: theme.palette.primary.main, textAlign: "center" }}
-        />
-        <CardContent>
-            <div>
-                <Button
-                    style={{marginInline: "2vw"}}                      
-                    color="secondary"
-                    variant="contained"
-                    onClick={travelerClicked}
-                >
-                Traveler
-                </Button>
-                <Button
-                    style={{marginInline: "2vw"}}
-                    color="secondary"
-                    variant="contained"
-                    onClick={regionClicked}
+
+            );
+            
+
+        }
+        else
+        {
+            return null;
+        }
+        }, [state.selectedOption]);
+    
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Card className="card">
+            <Project1Component  />
+            <CardHeader
+                title="List Advisories"
+                style={{ color: theme.palette.primary.main, textAlign: "center" }}
+            />
+            <CardContent>
+                <div>
+                    <Button
+                        style={{marginInline: "2vw"}}                      
+                        color="secondary"
+                        variant="contained"
+                        onClick={travelerClicked}
                     >
-                    Region
-                </Button>
-                <Button
-                    style={{marginInline: "2vw"}}
-                    color="secondary"
-                    variant="contained"
-                    onClick={subregionClicked}
-                    >
-                    Sub-Region
-                </Button>
-            </div>
-            <div>
-                <Autocomplete
-                    options={state.autoOptions}
-                    getOptionLabel={(option) => option}
-                    style={{ padding: "1vh" }}
-                               onChange={onChange}
-                               value={state.selectedOption}
-                               renderInput={(params) => (
-                               <TextField
-                                   {...params}
-                                   label={state.autoLabel}
-                                   variant="outlined"
-                                   fullWidth
-                                   
-                               />
-                               )}
-                           />
-            </div>
-        </CardContent>
-        </Card>
-    </ThemeProvider>
-);
-
-
-
+                    Traveler
+                    </Button>
+                    <Button
+                        style={{marginInline: "2vw"}}
+                        color="secondary"
+                        variant="contained"
+                        onClick={regionClicked}
+                        >
+                        Region
+                    </Button>
+                    <Button
+                        style={{marginInline: "2vw"}}
+                        color="secondary"
+                        variant="contained"
+                        onClick={subregionClicked}
+                        >
+                        Sub-Region
+                    </Button>
+                </div>
+                <div>
+                    <Autocomplete
+                        options={state.autoOptions}
+                        getOptionLabel={(option) => option}
+                        style={{ padding: "1vh" }}
+                                onChange={onChange}
+                                value={state.selectedOption}
+                                renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label={state.autoLabel}
+                                    variant="outlined"
+                                    fullWidth
+                                />
+                                )}
+                            />
+                </div>
+                <div>
+            {renderContent()}
+        </div>
+            </CardContent>
+            </Card>
+        </ThemeProvider>
+    );
 };
 export default AdvisoryListComponent;
